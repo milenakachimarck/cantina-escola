d2c87c8092d994d4fbc6006b68de053cfde3e289
milenak <milena.kachimarck@escola.pr.gov.br>
2025-09-18 11:12:02 -0300

    .

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+var nomeCantina = "Cantina da Escola";
+console.log("Bem-vindo à " + nomeCantina);
+let salgados = 20;
+console.log("Temos " + salgados + " salgados disponíveis.");
+const precoSalgado = 5;
+console.log("Cada salgado custa R$" + precoSalgado);
+
 let.salgados= 20;
 console.log("Temos " + salgados + " salgados disponíveis.");
 const precoSalgado = 5;
