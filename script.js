4469761fcf2a83a47b754f3215e361d5c7673964
milenak <milena.kachimarck@escola.pr.gov.br>
2025-09-18 11:03:07 -0300

    .

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+let.salgados= 20;
+console.log("Temos " + salgados + " salgados disponíveis.");
+const precoSalgado = 5;
+console.log("Cada salgado custa R$" + precoSalgado);
+salgados = salgados - 5;let totalVendido = 5 * precoSalgado;
 
+console.log("Agora restam " + salgados + " salgados.");
+console.log("A cantina vendeu R$" + totalVendido);
+if (true) {
+var testeVar = "Sou var"; 
+ let testeLet = "Sou let"; 
+ console.log(testeVar); // funciona 
+ console.log(testeLet); // funciona
+} 
+console.log(testeVar); // funciona 
+console.log(testeLet); // ERRO
